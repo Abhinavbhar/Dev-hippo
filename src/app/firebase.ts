b3cce1d2c4211dd2006b4392cc42e59357b6f002
abhinav <abhinavbhar2004@gmail.com>
2023-12-11 22:54:42 +0530

    first commit

---
+++ src/app/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0HMu-ysbGE5IRGjXETQcho8ocwSKujd0",
+  authDomain: "dev-hippo-7a541.firebaseapp.com",
+  projectId: "dev-hippo-7a541",
+  storageBucket: "dev-hippo-7a541.appspot.com",
+  messagingSenderId: "65004316970",
+  appId: "1:65004316970:web:ad75c799948112f42f1653"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/config/index.ts
@@ -0,0 +1,42 @@
+export const PRODUCT_CATEGORIES = [{
+  label: "Icons",
+  value: "icons" as const,
+  featured: [{
+    name: "favourite icons",
+    href: '#',
+    imageSrc: '/nav/icons/picks.jpg',
+  },
+  {
+    name: "New arrivals ",
+    href: '#',
+    imageSrc: '/nav/icons/new.jpg',
+  },
+  {
+    name: "Bestselling icons",
+    href: '#',
+    imageSrc: '/nav/icons/bestsellers.jpg',
+  }
+  ]
+
+},
+{
+  label: "Ui-kits",
+  value: "ui-kits" as const,
+  featured: [{
+    name: "favourite ui-kits",
+    href: '#',
+    imageSrc: '/nav/ui-kits/blue.jpg',
+  },
+  {
+    name: "New arrivals ",
+    href: '#',
+    imageSrc: '/nav/ui-kits/mixed.jpg',
+  },
+  {
+    name: "Bestselling ui",
+    href: '#',
+    imageSrc: '/nav/ui-kits/purple.jpg',
+  }
+  ]
+
+}]
